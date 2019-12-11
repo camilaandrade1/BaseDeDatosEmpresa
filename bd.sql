0c971a1b806163bb84cb84a37ae50db3abc42419
camilaandrade1 <51163561+camilaandrade1@users.noreply.github.com>
2019-12-11 15:41:21 -0500

    version 2.0

--- bd.sql
+++ bd.sql
@@ -6,9 +6,9 @@ create table empleado(
 );
 
 insert into empleado (id,nombre,n_boleta,n_equipo_id) values
-(1, 'Camila', 45, 76)
-(2, 'Hellen', 58, 87)
-(3, 'Sofia', 97, 51)
-(4, 'Camilo', 35, 34)
-(5, 'Carlos', 67, 98)
-(6, 'John', 25, 46)
+(1, 'Camila', 45, 76),
+(2, 'Hellen', 58, 87),
+(3, 'Sofia', 97, 51),
+(4, 'Camilo', 35, 34),
+(5, 'Carlos', 67, 98),
+(6, 'John', 25, 46);
